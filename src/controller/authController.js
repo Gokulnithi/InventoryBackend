ac131b8aae313680be37030bdd400a5fce2b2f67
Gokulnithi <gokulnithi.p.mts@gmail.com>
2025-08-08 02:01:38 +0530

    sameSite updated!

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -46,7 +46,7 @@ const login = async (req, res) => {
         httpOnly:true,
         secure: process.env.NODE_ENV === "production",
         maxAge: 24*60*60*1000,
-        sameSite:"strict",
+        sameSite:"none",
     })
 
     res.status(200).json({token,role:user.role,id:user._id})
